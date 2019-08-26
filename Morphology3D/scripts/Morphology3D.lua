50917e5cf18c53c0766d4a171833534693c857a1
Florian Roser <Florian.Roser@sick.de>
2019-08-26 18:43:06 +0200

    'Update'

--- Morphology3D/scripts/Morphology3D.lua
+++ Morphology3D/scripts/Morphology3D.lua
@@ -2,10 +2,10 @@
 
   Application Name:
   Morphology3D
-                                                                                             
+
   Description:
   Applying Morphology filter on a 3D image.
-   
+
   How to Run:
   Starting this sample is possible either by running the app (F5) or
   debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
@@ -13,7 +13,7 @@
   Results can be seen in the viewer on the DevicePage.
   Select Reflectance in the View: box at the top of the GUI and zoom in on the
   data for best experience.
-       
+
   More Information:
   Tutorial "Algorithms - Filtering and Arithmetic".
 
@@ -29,6 +29,8 @@ viewer1:setID('viewer1')
 local viewer2 = View.create()
 viewer2:setID('viewer2')
 
+local DELAY = 2000
+
 --End of Global Scope-----------------------------------------------------------
 
 -- Start of Function and Event Scope--------------------------------------------
@@ -39,9 +41,13 @@ local function filteringImage(heightMap, intensityMap)
   -- The possible transformation types are: ERODE, DILATE, OPEN, CLOSE,
   -- GRADIENT, TOPHAT and BLACKHAT
 
+  local hmDeco = View.ImageDecoration.create()
+  local minR, maxR = Image.getMinMax(heightMap)
+  hmDeco:setRange(minR, maxR)
+
   -- Visualize the input (original image)
   viewer1:clear()
-  viewer1:addHeightmap({heightMap, intensityMap}, {}, {'Reflectance'})
+  viewer1:addHeightmap({heightMap, intensityMap}, {hmDeco}, {'Reflectance'})
   viewer1:present()
 
   Script.notifyEvent('OnMessage1', 'Original image')
@@ -55,23 +61,27 @@ local function filteringImage(heightMap, intensityMap)
   local erodeIntensityMap = intensityMap:morphology(diameterPix, TRANSFORMATION)
   -- Visualize the output (median image)
   viewer2:clear()
-  viewer2:addHeightmap({erodeImage, erodeIntensityMap}, {}, {'Reflectance'})
+  viewer2:addHeightmap({erodeImage, erodeIntensityMap}, {hmDeco}, {'Reflectance'})
   viewer2:present()
 
   Script.notifyEvent('OnMessage2', 'Morphology filter, diameter pixel: ' .. diameterPix .. ', transformation: ' .. TRANSFORMATION)
 end
 
 local function main()
-  -- Load a json-image
-  local data = Object.load('resources/image_23.json')
+  for i = 1, 1 do
+    -- Load a json-image
+    local data = Object.load('resources/image_' .. i .. '.json')
 
-  -- Extract heightmap, intensity map and sensor data
-  local heightMap = data[1]
-  local intensityMap = data[2]
-  local sensorData = data[3] --luacheck: ignore
+    -- Extract heightmap, intensity map and sensor data
+    local heightMap = data[1]
+    local intensityMap = data[2]
+    local sensorData = data[3] --luacheck: ignore
 
-  -- Filter image
-  filteringImage(heightMap, intensityMap)
+    -- Filter image
+    filteringImage(heightMap, intensityMap)
+
+    Script.sleep(DELAY)
+  end
 
   print('App finished')
 end
