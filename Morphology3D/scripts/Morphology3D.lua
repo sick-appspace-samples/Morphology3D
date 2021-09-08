2fd8e2a7c47ccc15a117a2a1f5810b4a4280a375
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-09-08 12:51:25 +0200

    'Update'

--- Morphology3D/scripts/Morphology3D.lua
+++ Morphology3D/scripts/Morphology3D.lua
@@ -1,23 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name:
-  Morphology3D
-
-  Description:
-  Applying Morphology filter on a 3D image.
-
-  How to Run:
-  Starting this sample is possible either by running the app (F5) or
-  debugging (F7+F10). Setting breakpoint on the first row inside the 'main'
-  function allows debugging step-by-step after 'Engine.OnStarted' event.
-  Results can be seen in the viewer on the DevicePage.
-  Select Reflectance in the View: box at the top of the GUI and zoom in on the
-  data for best experience.
-
-  More Information:
-  Tutorial "Algorithms - Filtering and Arithmetic".
-
-------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 
 Script.serveEvent('Morphology3D.OnMessage1', 'OnMessage1', 'string')
